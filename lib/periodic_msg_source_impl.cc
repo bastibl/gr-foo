85de8f71dcb91624289eb68931f2e9288f5e8e8f
Bastian Bloessl <bloessl@ccs-labs.org>
2015-10-24 01:09:31 +0200

    message source: notify shutdown also w/ EOF

--- lib/periodic_msg_source_impl.cc
+++ lib/periodic_msg_source_impl.cc
@@ -67,6 +67,7 @@ periodic_msg_source_impl::run(periodic_msg_source_impl *instance) {
 				d_finished = true;
 				if(d_quit) {
 					boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+					message_port_pub( pmt::mp("out"), pmt::PMT_EOF);
 					post(pmt::mp("system"), pmt::cons(pmt::mp("done"), pmt::from_long(1)));
 				}
 				break;
@@ -91,6 +92,7 @@ periodic_msg_source_impl::run(periodic_msg_source_impl *instance) {
 		d_finished = true;
 		if(d_quit) {
 			boost::this_thread::sleep(boost::posix_time::milliseconds(500));
+			message_port_pub( pmt::mp("out"), pmt::PMT_EOF);
 			post(pmt::mp("system"), pmt::cons(pmt::mp("done"), pmt::from_long(1)));
 		}
 	}
